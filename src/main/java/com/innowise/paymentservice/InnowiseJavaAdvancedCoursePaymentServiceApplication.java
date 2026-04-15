b06ddd5ebb427c77cab3c35bab7ab382caf1e4a2
tabletka97 <khatkevichmatsvei@gmail.com>
2026-04-15 17:48:07 +0300

    init commit

---
+++ src/main/java/com/innowise/paymentservice/InnowiseJavaAdvancedCoursePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowiseJavaAdvancedCoursePaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnowiseJavaAdvancedCoursePaymentServiceApplication.class, args);
+    }
+
+}
