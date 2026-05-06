b21481be89a824d615b558306f3e832ae6728ed5
tabletka97 <khatkevichmatsvei@gmail.com>
2026-05-06 23:45:22 +0300

    fix: dto sending

--- src/main/java/com/innowise/paymentservice/messagebrokers/OutboxEventSender.java
+++ src/main/java/com/innowise/paymentservice/messagebrokers/OutboxEventSender.java
@@ -1,7 +1,10 @@
 package com.innowise.paymentservice.messagebrokers;
 
 import com.innowise.paymentservice.repository.CreatePaymentEventRepository;
+import com.innowise.paymentservice.repository.PaymentRepository;
 import com.innowise.paymentservice.repository.entity.CreatePaymentEvent;
+import com.innowise.paymentservice.repository.entity.Payment;
+import com.innowise.paymentservice.service.mappers.PaymentMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,9 +19,11 @@ public class OutboxEventSender {
 
     private final MessageBroker messageBroker;
     private final CreatePaymentEventRepository eventRepository;
+    private final PaymentRepository paymentRepository;
+    private final PaymentMapper mapper;
 
     @Scheduled(fixedRate = 1000)
-    private void  sendMessages() {
+    private void sendMessages() {
         List<CreatePaymentEvent> all = eventRepository.findAll();
 
         if (all.isEmpty()) {
@@ -27,8 +32,17 @@ public class OutboxEventSender {
 
         log.info("Starting event processing:");
 
+        List<Payment> payments = paymentRepository.findAllById(
+                all.stream().map(CreatePaymentEvent::getPaymentId).toList()
+        );
+
         for (CreatePaymentEvent event : all) {
-            if (messageBroker.sendMessage(event)) {
+            var payment = payments.stream()
+                    .filter(p -> p.getId().equals(event.getPaymentId()))
+                    .findFirst()
+                    .get();
+
+            if (messageBroker.sendMessage(mapper.entityToDto(payment))) {
                 eventRepository.delete(event);
             } else {
                 log.info("Event failed to send. Stopping event processing.");
