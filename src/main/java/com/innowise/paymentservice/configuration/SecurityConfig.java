d7a5801ca4651702c822190e1e2cf64faf0e8afd
tabletka97 <khatkevichmatsvei@gmail.com>
2026-05-06 14:42:31 +0300

    fix: open access to readiness probe

--- src/main/java/com/innowise/paymentservice/configuration/SecurityConfig.java
+++ src/main/java/com/innowise/paymentservice/configuration/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                                         })
                 )
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/actuator/health/liveness").permitAll()
+                        .requestMatchers("/actuator/health/*").permitAll()
                         .anyRequest().authenticated()
                 )
                 .build();
